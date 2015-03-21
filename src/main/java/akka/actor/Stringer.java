5859ffcf19587bd085586be4d2680afcd9f32bd8
rekkeb <rekkeb@gmail.com>
2015-03-21 12:32:57 +0100

    Implemented new string split and reverse behavior

--- src/main/java/akka/actor/Stringer.java
+++ src/main/java/akka/actor/Stringer.java
@@ -1,12 +1,58 @@
 package akka.actor;
 
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+import akka.message.Message;
+
+import java.util.Arrays;
+
 /**
- * Created by Rekeb on 21/03/15.
+ * Actor that receives a String, splits it and sends the chunks
+ * to Reverser actors
  */
 public class Stringer extends UntypedActor {
 
+    private final LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+
+    private String data;
+    private int numberOfChunks = 0;
+    private int chunksProcessed = 0;
+
+    public Stringer(String data) {
+        this.data = data;
+    }
+
     @Override
     public void onReceive(Object msg) throws Exception {
+        if (msg == Message.SPLIT){
+            //Splits the data and sends each of the chunks to a new actor
+            String[] dataSplit = data.split(" ");
+            numberOfChunks = dataSplit.length;
+            log.info("Split: {}", Arrays.toString(dataSplit));
 
+            for (String chunk : dataSplit){
+                getContext().actorOf(Props.create(Reverser.class)).tell(chunk, getSelf());
+            }
+
+            //getSender().tell(Message.DONE, getSelf());
+            //getContext().stop(getSelf());
+        }
+        if (msg == Message.DONE){
+            chunksProcessed++;
+            if(numberOfChunks == chunksProcessed){
+                log.info("String has been processed");
+
+                //getContext().stop(getSelf());
+
+                getContext().system().shutdown();
+            }
+        }
+        else{
+            unhandled(msg);
+        }
+    }
+
+    public String getData() {
+        return data;
     }
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,11 +1,24 @@
 package main;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.actor.Stringer;
+import akka.message.Message;
+
 /**
  *
  */
 public class Main {
 
     public static void main(String[] args) {
+
+        final ActorSystem actorSystem = ActorSystem.create("helloakka");
+        final ActorRef stringer = actorSystem.actorOf(Props.create(Stringer.class, "This string is going to be split and reversed"), "stringer");
+
+        stringer.tell(Message.SPLIT, ActorRef.noSender());
+
+        //actorSystem.shutdown();
 
     }
 
