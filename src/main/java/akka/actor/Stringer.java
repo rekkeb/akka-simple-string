c024363f47e6698c8680b9c49ee0a76818e8c0d2
rekkeb <rekkeb@gmail.com>
2015-03-21 10:19:20 +0100

    Initial main class and Stringer actor

---
+++ src/main/java/akka/actor/Stringer.java
@@ -0,0 +1,12 @@
+package akka.actor;
+
+/**
+ * Created by Rekeb on 21/03/15.
+ */
+public class Stringer extends UntypedActor {
+
+    @Override
+    public void onReceive(Object msg) throws Exception {
+
+    }
+}
